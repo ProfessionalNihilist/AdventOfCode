ede3a803d05b7ed303f23acf6278dd0ea0268a52
Isobel Cullen <646896+isobel-cullen@users.noreply.github.com>
2021-11-25 09:42:32 +0000

    solution to day 1 part 2

--- src/AdventOfCode/Program.fs
+++ src/AdventOfCode/Program.fs
@@ -8,11 +8,17 @@ open FSharp.Data
 let main argv =
     let input = Http.RequestString( "https://adventofcode.com/2019/day/1/input",
                                     cookieContainer = AoC.Utilities.authCookieContainer)
+
+    let rec fuelForMass m =
+        let fm = Math.Max(0, (m / 3) - 2)
+        if fm = 0 then 0
+        else fm + fuelForMass fm
+
     let fuel = input.Split("\n")
                 |> Seq.map Int32.TryParse
                 |> Seq.filter fst
                 |> Seq.map snd
-                |> Seq.sumBy (fun m -> (m / 3) - 2)
+                |> Seq.sumBy fuelForMass
 
     printfn "Total fuel requirement: %d" fuel
 
