052aff564c96c61680c5ab75f18af3340ab4067c
Isobel Cullen <646896+isobel-cullen@users.noreply.github.com>
2021-11-25 09:42:33 +0000

    day 1 part 1

--- src/Year/2020/Day/Day1.fs
+++ src/Year/2020/Day/Day1.fs
@@ -1,7 +1,25 @@
 ﻿module Year2020.Day1
 
 open AdventOfCode
+open System
 
 let solution: Solution = fun getInput ->
+    let isExpense x y =
+        match x + y with
+        | 2020 -> x * y |> Some
+        | _ -> None
+
+    let splitOpts = StringSplitOptions.RemoveEmptyEntries ||| StringSplitOptions.TrimEntries
+    let expenses = (getInput ()).Split("\n", splitOpts)
+                    |> Seq.map (Int32.Parse)
+
+    let expense = expenses
+                |> Seq.tryPick (fun x ->
+                    expenses |> Seq.tryPick (isExpense x))
+
+    match expense with
+    | Some e -> printfn "Expense is %d" e
+    | None -> printfn "No valid expense found?"
+
     ()
 
