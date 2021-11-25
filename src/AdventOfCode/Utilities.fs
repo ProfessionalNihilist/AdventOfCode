0efea380024652ee6dd0a433bd17e76502923f07
Isobel Cullen <646896+isobel-cullen@users.noreply.github.com>
2021-11-25 09:42:32 +0000

    Cache inputs on filesystem

--- src/AdventOfCode/Utilities.fs
+++ src/AdventOfCode/Utilities.fs
@@ -1,4 +1,5 @@
 module AdventOfCode.Input
+    open System.IO
     open System.Net
     open FSharp.Data
 
@@ -6,11 +7,18 @@ module AdventOfCode.Input
         let cookies = CookieContainer()
         let auth = Cookie(
                     "session",
-                    "")
+                    "53616c7465645f5fa63391ac93bd62b7c63de0defa64ec18e07392959b69f03fe28537930c4fe2c922a0e75e25ad0ac0")
         auth.Domain <- "adventofcode.com"
         cookies.Add(auth)
         cookies
 
     let inputForDay day =
-        let uri = sprintf "https://adventofcode.com/2019/day/%d/input" day
-        fun () -> Http.RequestString(uri, cookieContainer = authCookieContainer)
+        let filename = sprintf "./inputs/%d" day
+        match File.Exists filename with
+        | false ->
+            let uri = sprintf "https://adventofcode.com/2019/day/%d/input" day
+            let input = Http.RequestString(uri, cookieContainer = authCookieContainer)
+            File.WriteAllText(filename, input)
+            fun () -> input
+        | true ->
+            fun () -> File.ReadAllText filename
