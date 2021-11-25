81029159dc4a56e2f3a68ac04b69a877a335d955
Isobel Cullen <646896+isobel-cullen@users.noreply.github.com>
2021-11-25 09:42:33 +0000

    day 2 part 2

--- src/Year/2020/Day/Day2.fs
+++ src/Year/2020/Day/Day2.fs
@@ -3,7 +3,9 @@
 open System
 
 let passwordPhilosophy (getInput: unit -> string) =
-    let matchesRule (rule: string) =
+    let input  = (getInput ()).Split("\n", StringSplitOptions.RemoveEmptyEntries)
+
+    let parseRule (rule: string) =
         let mutable i = 0
 
         let reader = seq {
@@ -19,15 +21,31 @@ let passwordPhilosophy (getInput: unit -> string) =
         let min = readInt reader
         let max = reader |> Seq.skip 1 |> readInt
         let character = reader |> Seq.skip 1 |> Seq.head
-        let password = reader |> Seq.skip 2 |> String.Concat
+        let password = reader |> Seq.skip 3 |> String.Concat
+        (min,max,character,password)
 
+    let matchesMinMaxRule (min,max,character,password) =
         let chars = password |> Seq.where ((=) character) |> Seq.length
         chars >= min && chars <= max
 
-    let valid = (getInput ()).Split("\n", StringSplitOptions.RemoveEmptyEntries)
-                |> Seq.where matchesRule
-                |> Seq.length
+    let matchesPositionRule ((p1,p2,character,password): int * int * char * string) =
+        match p2 > password.Length with
+        | true when p1 > password.Length -> false
+        | true -> password.[p1 - 1] = character
+        | false ->
+            match (password.[p1 - 1] = character), (password.[p2 - 1] = character) with
+            | true, true -> false
+            | false, false -> false
+            | _ -> true
 
-    printfn "%d valid passwords" valid
+    let validMinMax =
+        input
+        |> Seq.where (parseRule >> matchesMinMaxRule)
+        |> Seq.length
 
+    let validPositional =
+        input
+        |> Seq.where (parseRule >> matchesPositionRule)
+        |> Seq.length
 
+    printfn "%d valid passwords" validPositional
